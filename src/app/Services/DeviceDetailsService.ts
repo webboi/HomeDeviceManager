6fba39cc93ea41a3943d1bef97a2b1dd44d3621f
webboi <isaacwebbo189@gmail.com>
2023-12-15 14:59:32 +0300

    Updated Styles

--- src/app/Services/DeviceDetailsService.ts
+++ src/app/Services/DeviceDetailsService.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Device } from "../Model/device";
+import { Device } from '../Model/device';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DeviceDetailService {
+  private apiUrl = 'https://localhost:7122/api/DeviceDetails';
 
-  private apiUrl = 'https://localhost:7122/api/DeviceDetails'; 
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getDevices(): Observable<Device[]> {
     return this.http.get<Device[]>(this.apiUrl);
   }
-  
-    
+
+  addDevice(device: Device) {
+    return this.http.post(this.apiUrl, device);
   }
 
+  editDevice(device: Device){
+    return this.http.put(`${this.apiUrl}/${device.deviceId}` , device);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { DeviceDetailService } from './Services/DeviceDetailsService';
 import { Device } from './Model/device';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,14 +18,53 @@ export class AppComponent implements OnInit{
 
   devices: Device[]=[];
 
+  deviceName: string | undefined;
+  deviceType: string | undefined;
+  deviceExpDate: string | undefined;
+
+  message :string| undefined;
+
   constructor(private deviceService: DeviceDetailService ) { }
 
   ngOnInit(): void {
+  this.getDevices();
+  }
+  
+  createDevice(){
+    if(this.deviceName && this.deviceExpDate && this.deviceType){
+      const params: Device = {
+        deviceId: 0,
+        deviceName: this.deviceName,
+        type: this.deviceType,
+        expirationDate: this.deviceExpDate,
+        isOn: false
+      }
+      this.deviceService.addDevice(params).subscribe(() => {
+        this.getDevices();
+        this.message = 'Device created successfully';
+        this.deviceExpDate = '';
+        this.deviceName = '';
+        this.deviceType = '';
+      })
+    }else{
+      this.message = 'All fields are required';
+    }
+   
+  }
+
+  getDevices(){
     this.deviceService.getDevices().subscribe(devices => {
       this.devices = devices;
     });
   }
-  
+
+
+  toggleDevice(device: Device){
+    const params:Device = {...device, isOn:!device.isOn};
+    this.deviceService.editDevice(params).subscribe(()=>{
+      this.getDevices();
+    })
+  }
 
 }
 
